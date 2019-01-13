44ef8cacf668cce40fd9d36533d6b3056f9603a2
Jason Hiskey <jasonhi@crazyredhead.com>
2019-01-12 16:17:33 -0800

    Completed GivenABoardTestForWinRow

--- Lab04_TicTacToe/Lab04_TicTacToeTest/UnitTest1.cs
+++ Lab04_TicTacToe/Lab04_TicTacToeTest/UnitTest1.cs
@@ -7,9 +7,44 @@ namespace Lab04_TicTacToeTest
     public class GameTest
     {
         [Fact]
-        public void GivenABoardTestForWinners()
+        public void GivenABoardTestForWinRow()
         {
-            
+            //Sets the Game Board Selected Positions
+            Position positionOne = new Position(0, 0);
+            Position positionTwo = new Position(0, 1);
+            Position positionThree = new Position(0, 2);
+
+            // Sets the Players of the Game
+            Player player1 = new Player();
+            Player player2 = new Player();
+
+            //Initiates a new game
+            Game testGame = new Game(player1, player2);
+
+            //Marks Game Board Selected Positions
+            testGame.Board.GameBoard[positionOne.Row, positionOne.Column] = "X";
+            testGame.Board.GameBoard[positionTwo.Row, positionTwo.Column] = "X";
+            testGame.Board.GameBoard[positionThree.Row, positionThree.Column] = "X";
+
+            Assert.True(testGame.CheckForWinner(testGame.Board));
+        }
+
+        [Fact]
+        public void GivenABoardTestForWinColumn()
+        {
+
+        }
+
+        [Fact]
+        public void GivenABoardTestForWinDiag()
+        {
+
+        }
+
+        [Fact]
+        public void GivenABoardTestForNoWin()
+        {
+
         }
 
         [Fact]
@@ -24,10 +59,6 @@ namespace Lab04_TicTacToeTest
 
         }
 
-        [Fact]
-        public void TestThatGameEndsAfterTurnNine()
-        {
-
-        }
+      
     }
 }
