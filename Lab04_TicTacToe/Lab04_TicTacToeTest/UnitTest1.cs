fcef1be6b421428d44bb8dbe9e6a0cd19fa19e33
Jason Hiskey <jasonhi@crazyredhead.com>
2019-01-12 16:19:23 -0800

    Completed GivenABoardTestForWinColumn

--- Lab04_TicTacToe/Lab04_TicTacToeTest/UnitTest1.cs
+++ Lab04_TicTacToe/Lab04_TicTacToeTest/UnitTest1.cs
@@ -32,7 +32,24 @@ namespace Lab04_TicTacToeTest
         [Fact]
         public void GivenABoardTestForWinColumn()
         {
+            //Sets the Game Board Selected Positions
+            Position positionOne = new Position(0, 0);
+            Position positionTwo = new Position(1, 0);
+            Position positionThree = new Position(2, 0);
 
+            // Sets the Players of the Game
+            Player player1 = new Player();
+            Player player2 = new Player();
+
+            //Initiates a new game
+            Game testGame = new Game(player1, player2);
+
+            //Marks Game Board Selected Positions
+            testGame.Board.GameBoard[positionOne.Row, positionOne.Column] = "X";
+            testGame.Board.GameBoard[positionTwo.Row, positionTwo.Column] = "X";
+            testGame.Board.GameBoard[positionThree.Row, positionThree.Column] = "X";
+
+            Assert.True(testGame.CheckForWinner(testGame.Board));
         }
 
         [Fact]
